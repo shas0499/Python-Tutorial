578b4544fd673cad9ff6838ea15ca3c3d96fcecd
Shaswata <shaswata.barua0499@outlook.com>
2025-11-04 19:33:53 +0530

    Add files via upload

---
+++ Loop/While Loop/Format.py
@@ -0,0 +1,13 @@
+print("---------------Increment---------------")
+
+i = 1
+while i<=10:
+    print(i)
+    i = i+1
+
+print("----------------Decrement--------------")
+
+i = 10
+while i>0:
+    print(i)
+    i = i -1
