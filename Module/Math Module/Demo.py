191e5f9cf7057d2296907c0637a0553c1c31944f
Shaswata <shaswata.barua0499@outlook.com>
2025-11-10 21:40:37 +0530

    Add files via upload

---
+++ Module/Math Module/Demo.py
@@ -0,0 +1,21 @@
+import math
+
+x = 11.3
+print(math.ceil(x))
+
+y = 13
+print(math.ceil(y))
+
+z = -13
+print(math.fabs(z))
+
+x = 5
+print(math.factorial(x))
+
+y = 11.5
+print(math.floor(y))
+
+lst = [10,30,40]
+print(math.fsum(lst))
+
+print(math.sqrt(16))
